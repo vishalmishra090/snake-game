31e39b75c49f3bcbe1a618d9f60bf41bf3c0ffa9
Vishal Mishra <vishalmishra090@gmail.com>
2020-10-19 21:42:36 +0530

    game complete

--- script/snake-game.js
+++ script/snake-game.js
@@ -1,9 +1,3 @@
-$("#node1").css({
-  top: "40%",
-  left: "48%",
-});
-
-let front = "null";
 const UP = -4;
 const LEFT = -4;
 const DOWN = +4;
@@ -14,18 +8,27 @@ let prevCssTop = null;
 let prevCssLeft = null;
 let nextCssTop = null;
 let nextCssLeft = null;
+let speed = 250;
+let timerId = null
 
-let snake = [
-  {
+let snake = [];
+
+let targetNode = {};
+
+function setGame() {
+  snake[0] = {
     cssTop: 40,
     cssLeft: 48,
-  },
-];
+  };
 
-let targetNode = {
-  cssTop: null,
-  cssLeft: null,
-};
+  targetNode.cssTop = null;
+  targetNode.cssLeft = null;
+
+  $("#gameArea").append(
+    '<div id="targetNode" class="nodes"></div>',
+    '<div class="nodes snakeNodes" id="node1" style="top: 40%; left: 48%; display: block;"></div>'
+  );
+}
 
 function changeTargetNodePos() {
   let rndmTop = Math.floor(Math.random() * 25);
@@ -37,6 +40,8 @@ function changeTargetNodePos() {
   targetNode.cssTop = rndmTop * 4;
   targetNode.cssLeft = rndmLeft * 4;
 }
+
+// Snake move
 
 function moveUp() {
   prevCssTop = snake[totalNode - 1].cssTop;
@@ -99,6 +104,8 @@ function moveSnake() {
   addNewNode();
 }
 
+// Update Previous Node Css
+
 function updatePrevNode() {
   for (let i = totalNode - 2; i >= 0; i--) {
     $("#node" + (i + 1)).css({
@@ -114,6 +121,8 @@ function updatePrevNode() {
   }
 }
 
+// Add new Node
+
 function addNewNode() {
   let frontNodeTop = snake[totalNode - 1].cssTop;
   let frontNodeLeft = snake[totalNode - 1].cssLeft;
@@ -121,87 +130,185 @@ function addNewNode() {
   let targetNodeLeft = targetNode.cssLeft;
 
   if (
-    (frontNodeTop == targetNodeTop &&
-      (frontNodeLeft == targetNodeLeft - 4 ||
-        frontNodeLeft == targetNodeLeft + 4)) ||
-    (frontNodeLeft == targetNodeLeft &&
-      (frontNodeTop == targetNodeTop - 4 ||
-        frontNodeTop == targetNodeTop + 4))
+    ((direction == "up" && frontNodeTop == targetNodeTop) ||
+      (direction == "down" && frontNodeTop == targetNodeTop)) &&
+    frontNodeLeft == targetNodeLeft
   ) {
-    $("#gamebox").append(
-      `<div class="nodes snakeNodes" id="node${totalNode + 1}" style="top:${targetNodeTop}%; left:${targetNodeLeft}%;"></div>`
-    );
-    totalNode++;
-    snake[totalNode-1] = {}
-    Object.assign(snake[totalNode-1],targetNode);
-    
-    changeTargetNodePos();
+    newNode(targetNodeTop, targetNodeLeft);
+    return;
   }
+
+  if (
+    ((direction == "left" && frontNodeLeft == targetNodeLeft) ||
+      (direction == "right" && frontNodeLeft == targetNodeLeft)) &&
+    frontNodeTop == targetNodeTop
+  ) {
+    newNode(targetNodeTop, targetNodeLeft);
+    return;
+  }
+}
+
+function newNode(targetNodeTop, targetNodeLeft) {
+  if (direction == "up") {
+    targetNodeTop = targetNodeTop - 4;
+  }
+  if (direction == "down") {
+    targetNodeTop = targetNodeTop + 4;
+  }
+  if (direction == "left") {
+    targetNodeLeft = targetNodeLeft - 4;
+  }
+  if (direction == "right") {
+    targetNodeLeft = targetNodeLeft + 4;
+  }
+  $("#gameArea").append(
+    `<div class="nodes snakeNodes" id="node${
+      totalNode + 1
+    }" style="top:${targetNodeTop}%; left:${targetNodeLeft}%;"></div>`
+  );
+  totalNode++;
+  snake[totalNode - 1] = {
+    cssTop: targetNodeTop,
+    cssLeft: targetNodeLeft,
+  };
+  changeTargetNodePos();
+  increaseSpeed();
+}
+
+// Speed
+
+function increaseSpeed() {
+  if (totalNode % 5 == 0 && speed > 50) {
+    speed = speed - 50;
+  }
+}
+
+// Settings
+
+function run() {
+  moveSnake();
+  if (gameOver(timerId)) {
+    return;
+  }
+  timerId = setTimeout(run, speed);
 }
 
 function startGame() {
+  setGame();
+  changeTargetNodePos();
   $(document).on("keydown", function (event) {
-    // console.log(event.keyCode);
-    //   console.log(event.key);
-
-    // ArrowLeft
-    if (event.keyCode == 37) {
-      direction = "left";
-    }
-
-    // ArrowUp
-    if (event.keyCode == 38) {
-      direction = "up";
-    }
-
-    // ArrowRight
-    if (event.keyCode == 39) {
-      direction = "right";
-    }
-
-    // ArrowDown
-    if (event.keyCode == 40) {
-      direction = "down";
-    }
+    if (event.keyCode == 37) direction = "left"; // ArrowLeft
+    if (event.keyCode == 38) direction = "up"; // ArrowUp
+    if (event.keyCode == 39) direction = "right"; //ArrowRight
+    if (event.keyCode == 40) direction = "down"; // ArrowDown
   });
-  let timerId = setInterval(function () {
-    moveSnake();
-    gameOver(timerId);
-  }, 250);
+  $("#pause").one("click", function () {
+    pauseGame();
+  });
+
+   timerId = setTimeout(run, speed);
+}
+
+$("#startGame").click(() => {
+  $("#home-container").css("display","none");
+  $("main").css("display", "block");
+  startGame();
+});
+
+$("#playAgain").click(() => playAgain());
+
+function playAgain() {
+  $("#playAgainBox").css("display", "none");
+  $("#restartBox").css("display", "none");
+  $("#gameArea").empty();
+   totalNode = 1;
+   direction = "up";
+   lprevCssTop = null;
+   prevCssLeft = null;
+   nextCssTop = null;
+   nextCssLeft = null;
+   speed = 250;
+   startGame();
 }
 
 function gameOver(timerId) {
-  let cssTop = snake[totalNode - 1].cssTop;
-  let cssLeft = snake[totalNode - 1].cssLeft;
-  if (cssTop > 96 || cssTop < 0 || cssLeft > 96 || cssLeft < 0) {
+  let firstNodeCssTop = snake[totalNode - 1].cssTop;
+  let firstNodeCssLeft = snake[totalNode - 1].cssLeft;
+
+  if (
+    firstNodeCssTop > 96 ||
+    firstNodeCssTop < 0 ||
+    firstNodeCssLeft > 96 ||
+    firstNodeCssLeft < 0
+  ) {
+    clearTimeout(timerId);
     $("#node" + totalNode).css("display", "none");
-    clearInterval(timerId);
     setTimeout(function () {
-      restart();
-    }, 50);
+      $("#playAgainBox").css("display","block");
+    }, 500);
+    return true;
   }
-}
 
-function restart() {
-  $("#node1").css({
-    top: "48%",
-    left: "48%",
-    display: "block",
-  });
-  snake[0].cssTop = 48;
-  snake[0].cssLeft = 48;
-  $(document).on("keydown", function (event) {
-    if (event.keyCode == 38) {
-      $(this).off();
-      startGame();
+  for (let i = totalNode - 2; i >= 0; i--) {
+    if (
+      firstNodeCssTop == snake[i].cssTop &&
+      firstNodeCssLeft == snake[i].cssLeft
+    ) {
+      clearTimeout(timerId);
+      setTimeout(function () {
+        $("#playAgainBox").css("display", "block");
+      }, 500);
+      return true;
     }
-  });
+  }
+  return false;
 }
 
-$(document).on("keydown", function (event) {
-  if (event.keyCode == 38) {
-    $(this).off();
-    changeTargetNodePos();
-    startGame();
-  }
+function pauseGame(){
+    if(timerId != null){
+        clearTimeout(timerId);
+    }
+    $("#pause").one("click", function(){
+        playGame();
+    })
+}
+
+function playGame(){
+    timerId = setTimeout(run, speed);
+    $("#pause").one("click", function () {
+      pauseGame();
+    });
+}
+
+function restartGame(){
+    $("main").css("display", "none");
+    $("#playAgainBox").css("display", "none");
+    $("#restartBox").css("display", "none");
+    $("#home-container").css("display", "block");
+    $("#pause").off();
+    $("#gameArea").empty();
+    totalNode = 1;
+    direction = "up";
+    lprevCssTop = null;
+    prevCssLeft = null;
+    nextCssTop = null;
+    nextCssLeft = null;
+    speed = 250;
+}
+
+
+$("#up").click(()=> (direction = "up"));
+$("#down").click(() => (direction = "down"));
+$("#left").click(() => (direction = "left"));
+$("#right").click(() => (direction = "right"));
+$("#restart").click(function(){
+    pauseGame();
+    $("#restartBox").css("display","block")
 });
+
+$("#ok").click(() => restartGame());
+$("#cancle").click(() => {
+    playGame();
+    $("#restartBox").css("display", "none")
+});
+
