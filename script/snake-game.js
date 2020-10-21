0a02bd0d6fde2ee64c2f4b2360d2a167fe768351
Vishal Mishra <vishalmishra090@gmail.com>
2020-10-21 15:35:09 +0530

    update addnewnode function (Now add in last)

--- script/snake-game.js
+++ script/snake-game.js
@@ -9,25 +9,35 @@ let prevCssLeft = null;
 let nextCssTop = null;
 let nextCssLeft = null;
 let speed = 250;
-let timerId = null
+let speedNo = 1;
+let timerId = null;
+let checkPlayAgain = false;
 
 let snake = [];
 
 let targetNode = {};
 
+
+let eatAudio = new Audio("../audio/eat.wav");
+let gameOverAudio = new Audio("../audio/gameOver.mp3");
+
 function setGame() {
   snake[0] = {
     cssTop: 40,
     cssLeft: 48,
+    nodeDirection: null,
   };
 
   targetNode.cssTop = null;
   targetNode.cssLeft = null;
+  targetNode.nodeDirection = null;
 
   $("#gameArea").append(
     '<div id="targetNode" class="nodes"></div>',
     '<div class="nodes snakeNodes" id="node1" style="top: 40%; left: 48%; display: block;"></div>'
   );
+  $("#speed").text(`x${speedNo}`);
+  $("#score").text(`1`);
 }
 
 function changeTargetNodePos() {
@@ -44,43 +54,47 @@ function changeTargetNodePos() {
 // Snake move
 
 function moveUp() {
-  prevCssTop = snake[totalNode - 1].cssTop;
-  prevCssLeft = snake[totalNode - 1].cssLeft;
-  $("#node" + totalNode).css({
-    top: snake[totalNode - 1].cssTop + UP + "%",
-    left: snake[totalNode - 1].cssLeft + "%",
+  prevCssTop = snake[0].cssTop;
+  prevCssLeft = snake[0].cssLeft;
+  $("#node1").css({
+    top: snake[0].cssTop + UP + "%",
+    left: snake[0].cssLeft + "%",
   });
-  snake[totalNode - 1].cssTop += UP;
+  snake[0].cssTop += UP;
+  snake[0].nodeDirection = "up";
 }
 
 function moveLeft() {
-  prevCssTop = snake[totalNode - 1].cssTop;
-  prevCssLeft = snake[totalNode - 1].cssLeft;
-  $("#node" + totalNode).css({
-    top: snake[totalNode - 1].cssTop + "%",
-    left: snake[totalNode - 1].cssLeft + LEFT + "%",
+  prevCssTop = snake[0].cssTop;
+  prevCssLeft = snake[0].cssLeft;
+  $("#node1").css({
+    top: snake[0].cssTop + "%",
+    left: snake[0].cssLeft + LEFT + "%",
   });
-  snake[totalNode - 1].cssLeft += LEFT;
+  snake[0].cssLeft += LEFT;
+  snake[0].nodeDirection = "left";
 }
 
 function moveRigth() {
-  prevCssTop = snake[totalNode - 1].cssTop;
-  prevCssLeft = snake[totalNode - 1].cssLeft;
-  $("#node" + totalNode).css({
-    top: snake[totalNode - 1].cssTop + "%",
-    left: snake[totalNode - 1].cssLeft + RIGHT + "%",
+  prevCssTop = snake[0].cssTop;
+  prevCssLeft = snake[0].cssLeft;
+  $("#node1").css({
+    top: snake[0].cssTop + "%",
+    left: snake[0].cssLeft + RIGHT + "%",
   });
-  snake[totalNode - 1].cssLeft += RIGHT;
+  snake[0].cssLeft += RIGHT;
+  snake[0].nodeDirection = "right";
 }
 
 function moveDown() {
-  prevCssTop = snake[totalNode - 1].cssTop;
-  prevCssLeft = snake[totalNode - 1].cssLeft;
-  $("#node" + totalNode).css({
-    top: snake[totalNode - 1].cssTop + DOWN + "%",
-    left: snake[totalNode - 1].cssLeft + "%",
+  prevCssTop = snake[0].cssTop;
+  prevCssLeft = snake[0].cssLeft;
+  $("#node1").css({
+    top: snake[0].cssTop + DOWN + "%",
+    left: snake[0].cssLeft + "%",
   });
-  snake[totalNode - 1].cssTop += DOWN;
+  snake[0].cssTop += DOWN;
+  snake[0].nodeDirection = "down";
 }
 
 function moveSnake() {
@@ -107,7 +121,7 @@ function moveSnake() {
 // Update Previous Node Css
 
 function updatePrevNode() {
-  for (let i = totalNode - 2; i >= 0; i--) {
+  for (let i = 1; i < totalNode; i++) {
     $("#node" + (i + 1)).css({
       top: prevCssTop + "%",
       left: prevCssLeft + "%",
@@ -116,16 +130,44 @@ function updatePrevNode() {
     nextCssLeft = prevCssLeft;
     prevCssTop = snake[i].cssTop;
     prevCssLeft = snake[i].cssLeft;
+    changeNodeDirectionValue(
+      nextCssTop,
+      nextCssLeft,
+      prevCssTop,
+      prevCssLeft,
+      i
+    );
     snake[i].cssTop = nextCssTop;
     snake[i].cssLeft = nextCssLeft;
+  }
+}
+
+function changeNodeDirectionValue(
+  nextCssTop,
+  nextCssLeft,
+  prevCssTop,
+  prevCssLeft,
+  nodePosInArr
+) {
+  if (nextCssTop < prevCssTop) {
+    snake[nodePosInArr].nodeDirection = "up";
+  }
+  if (nextCssTop > prevCssTop) {
+    snake[nodePosInArr].nodeDirection = "down";
+  }
+  if (nextCssLeft < prevCssLeft) {
+    snake[nodePosInArr].nodeDirection = "left";
+  }
+  if (nextCssLeft > prevCssLeft) {
+    snake[nodePosInArr].nodeDirection = "right";
   }
 }
 
 // Add new Node
 
 function addNewNode() {
-  let frontNodeTop = snake[totalNode - 1].cssTop;
-  let frontNodeLeft = snake[totalNode - 1].cssLeft;
+  let frontNodeTop = snake[0].cssTop;
+  let frontNodeLeft = snake[0].cssLeft;
   let targetNodeTop = targetNode.cssTop;
   let targetNodeLeft = targetNode.cssLeft;
 
@@ -143,43 +185,61 @@ function addNewNode() {
       (direction == "right" && frontNodeLeft == targetNodeLeft)) &&
     frontNodeTop == targetNodeTop
   ) {
-    newNode(targetNodeTop, targetNodeLeft);
+    newNode();
     return;
   }
 }
 
-function newNode(targetNodeTop, targetNodeLeft) {
-  if (direction == "up") {
-    targetNodeTop = targetNodeTop - 4;
+function newNode() {
+  eatAudio.play();
+  let lastNodeCssTop = snake[totalNode - 1].cssTop;
+  let lastNodeCssLeft = snake[totalNode - 1].cssLeft;
+  let lastNodeDirection = snake[totalNode - 1].nodeDirection;
+  let newNodeCssTop = null;
+  let newNodeCssLeft = null;
+  if (lastNodeDirection == "up") {
+    newNodeCssTop = lastNodeCssTop + 4;
+    newNodeCssLeft = lastNodeCssLeft;
   }
-  if (direction == "down") {
-    targetNodeTop = targetNodeTop + 4;
+  if (lastNodeDirection == "down") {
+    newNodeCssTop = lastNodeCssTop - 4;
+    newNodeCssLeft = lastNodeCssLeft;
   }
-  if (direction == "left") {
-    targetNodeLeft = targetNodeLeft - 4;
+  if (lastNodeDirection == "left") {
+    newNodeCssTop = lastNodeCssTop;
+    newNodeCssLeft = lastNodeCssLeft + 4;
   }
-  if (direction == "right") {
-    targetNodeLeft = targetNodeLeft + 4;
+  if (lastNodeDirection == "right") {
+    newNodeCssTop = lastNodeCssTop;
+    newNodeCssLeft = lastNodeCssLeft - 4;
   }
   $("#gameArea").append(
     `<div class="nodes snakeNodes" id="node${
       totalNode + 1
-    }" style="top:${targetNodeTop}%; left:${targetNodeLeft}%;"></div>`
+    }" style="top:${newNodeCssTop}%; left:${newNodeCssLeft}%;"></div>`
   );
-  totalNode++;
-  snake[totalNode - 1] = {
-    cssTop: targetNodeTop,
-    cssLeft: targetNodeLeft,
+
+  snake[totalNode] = {
+    cssTop: newNodeCssTop,
+    cssLeft: newNodeCssLeft,
+    nodeDirection: lastNodeDirection,
   };
+  totalNode++;
+  $("#score").text(`${totalNode}`);
   changeTargetNodePos();
-  increaseSpeed();
+  changeSpeed();
 }
 
 // Speed
 
-function increaseSpeed() {
-  if (totalNode % 5 == 0 && speed > 50) {
+function changeSpeed() {
+  if (totalNode % 7 == 0 && speed == 100 && speedNo == 4) {
+      speed = speed + 50;
+      $("#speed").text(`x${--speedNo}`);
+  }
+  if (totalNode % 5 == 0 && speed > 100) {
     speed = speed - 50;
+    $("#speed").text(`x${++speedNo}`);
   }
 }
 
@@ -196,21 +256,15 @@ function run() {
 function startGame() {
   setGame();
   changeTargetNodePos();
-  $(document).on("keydown", function (event) {
-    if (event.keyCode == 37) direction = "left"; // ArrowLeft
-    if (event.keyCode == 38) direction = "up"; // ArrowUp
-    if (event.keyCode == 39) direction = "right"; //ArrowRight
-    if (event.keyCode == 40) direction = "down"; // ArrowDown
-  });
+  addControl();
   $("#pause").one("click", function () {
     pauseGame();
   });
-
-   timerId = setTimeout(run, speed);
+  timerId = setTimeout(run, speed);
 }
 
 $("#startGame").click(() => {
-  $("#home-container").css("display","none");
+  $("#home-container").css("display", "none");
   $("main").css("display", "block");
   startGame();
 });
@@ -221,19 +275,33 @@ function playAgain() {
   $("#playAgainBox").css("display", "none");
   $("#restartBox").css("display", "none");
   $("#gameArea").empty();
-   totalNode = 1;
-   direction = "up";
-   lprevCssTop = null;
-   prevCssLeft = null;
-   nextCssTop = null;
-   nextCssLeft = null;
-   speed = 250;
-   startGame();
+  $("#pause").off();
+  totalNode = 1;
+  direction = "up";
+  lprevCssTop = null;
+  prevCssLeft = null;
+  nextCssTop = null;
+  nextCssLeft = null;
+  speed = 250;
+  speedNo = 1;
+  checkPlayAgain = false;
+  startGame();
+}
+
+function showPlayAgain(timerId) {
+  gameOverAudio.play();
+  clearTimeout(timerId);
+  $("#node1").css("display", "none");
+  $("#pause").off();
+  checkPlayAgain = true;
+  setTimeout(function () {
+    $("#playAgainBox").css("display", "block");
+  }, 500);
 }
 
 function gameOver(timerId) {
-  let firstNodeCssTop = snake[totalNode - 1].cssTop;
-  let firstNodeCssLeft = snake[totalNode - 1].cssLeft;
+  let firstNodeCssTop = snake[0].cssTop;
+  let firstNodeCssLeft = snake[0].cssLeft;
 
   if (
     firstNodeCssTop > 96 ||
@@ -241,74 +309,110 @@ function gameOver(timerId) {
     firstNodeCssLeft > 96 ||
     firstNodeCssLeft < 0
   ) {
-    clearTimeout(timerId);
-    $("#node" + totalNode).css("display", "none");
-    setTimeout(function () {
-      $("#playAgainBox").css("display","block");
-    }, 500);
+    showPlayAgain(timerId);
     return true;
   }
 
-  for (let i = totalNode - 2; i >= 0; i--) {
+  for (let i = 1; i < totalNode; i++) {
     if (
       firstNodeCssTop == snake[i].cssTop &&
       firstNodeCssLeft == snake[i].cssLeft
     ) {
-      clearTimeout(timerId);
-      setTimeout(function () {
-        $("#playAgainBox").css("display", "block");
-      }, 500);
+      showPlayAgain(timerId);
       return true;
     }
   }
   return false;
 }
 
-function pauseGame(){
-    if(timerId != null){
-        clearTimeout(timerId);
-    }
-    $("#pause").one("click", function(){
-        playGame();
-    })
+function pauseGame() {
+  removeControl();
+  if (timerId != null) {
+    clearTimeout(timerId);
+  }
+  $("#pause").one("click", function () {
+    playGame();
+  });
+  $("#pause").css("background-image", "url(../images/play.svg)");
 }
 
-function playGame(){
-    timerId = setTimeout(run, speed);
-    $("#pause").one("click", function () {
-      pauseGame();
-    });
+function playGame() {
+  addControl();
+  timerId = setTimeout(run, speed);
+  $("#pause").one("click", function () {
+    pauseGame();
+  });
+  $("#pause").css("background-image", "url(../images/pause.svg)");
 }
 
-function restartGame(){
-    $("main").css("display", "none");
-    $("#playAgainBox").css("display", "none");
-    $("#restartBox").css("display", "none");
-    $("#home-container").css("display", "block");
-    $("#pause").off();
-    $("#gameArea").empty();
-    totalNode = 1;
-    direction = "up";
-    lprevCssTop = null;
-    prevCssLeft = null;
-    nextCssTop = null;
-    nextCssLeft = null;
-    speed = 250;
+function removeControl() {
+  $("#up").off();
+  $("#down").off();
+  $("#left").off();
+  $("#right").off();
+  $(document).off("keydown");
 }
 
+function addControl() {
+  $("#up").click(() => (direction = "up"));
+  $("#down").click(() => (direction = "down"));
+  $("#left").click(() => (direction = "left"));
+  $("#right").click(() => (direction = "right"));
+  $(document).on("keydown", function (event) {
+    if (event.keyCode == 37) direction = "left"; // ArrowLeft
+    if (event.keyCode == 38) direction = "up"; // ArrowUp
+    if (event.keyCode == 39) direction = "right"; //ArrowRight
+    if (event.keyCode == 40) direction = "down"; // ArrowDown
+  });
+}
 
-$("#up").click(()=> (direction = "up"));
+function restartGame() {
+  $("main").css("display", "none");
+  $("#playAgainBox").css("display", "none");
+  $("#restartBox").css("display", "none");
+  $("#home-container").css("display", "block");
+  $("#pause").off();
+  $("#gameArea").empty();
+  totalNode = 1;
+  direction = "up";
+  lprevCssTop = null;
+  prevCssLeft = null;
+  nextCssTop = null;
+  nextCssLeft = null;
+  speed = 250;
+  speedNo = 1;
+  checkPlayAgain = false;
+}
+
+$("#up").click(() => (direction = "up"));
 $("#down").click(() => (direction = "down"));
 $("#left").click(() => (direction = "left"));
 $("#right").click(() => (direction = "right"));
-$("#restart").click(function(){
-    pauseGame();
-    $("#restartBox").css("display","block")
+$("#restart").click(function () {
+  if (checkPlayAgain == false) pauseGame();
+  $("#restartBox").css("display", "block");
 });
 
 $("#ok").click(() => restartGame());
 $("#cancle").click(() => {
-    playGame();
-    $("#restartBox").css("display", "none")
+  if (checkPlayAgain == false) playGame();
+  $("#restartBox").css("display", "none");
 });
 
+/* sound */
+
+function muteAudio(){
+    $("#sound").css("background-image", "url(../images/sound-off.svg)");
+    eatAudio.muted = true;
+    gameOverAudio.muted = true;
+    $("#sound").one("click", () => unmuteAudio());
+}
+
+function unmuteAudio(){
+   $("#sound").css("background-image", "url(../images/sound-on.svg)");
+   eatAudio.muted = false;
+   gameOverAudio.muted = false; 
+   $("#sound").one("click", () => muteAudio());
+}
+
+$("#sound").one("click", () => muteAudio());
